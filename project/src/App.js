52be3c5b894857b81e3dcb72f210177d7a483d74
zhangshuo <443494891@qq.com>
2019-11-25 12:09:16 +0800

    use project

---
+++ project/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd-mobile';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>button</Button>
+    </div>
+  );
+}
+
+export default App;
