5df0c84b252e84d1530d19509f549b6fa4fd940f
zhangshuo <443494891@qq.com>
2019-12-04 09:04:02 +0800

    add 个人信息页 新建笔记页

---
+++ project/server/api/addSionple.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var query = require('../db');
+const router = express.Router();
+// 新增笔记api
+
+router.post('/',async (req,res)=>{
+    // 请求中须携带的数据 uid，tags，title，context，isShare，添加图片的路径img_path
+    // 
+    const {uid,tags,title,context,isShare} = req.body;
+    // const uid = 'k3i297def';
+    // const tags = '测试2';
+    // const title = '测试测试';
+    // const context = '家乐鸡粉四间房了司法局未播放机无法焦距的v耳机架的更多见收购的精神电视剧定时关机搜被动句dove';
+    // const isShare = 1;
+    var chdate = new Date();// 创建笔记的时间
+
+    var tag_id = await query('select tag_id from tagsName where tags=?',[tags]);
+    tag_id = JSON.parse(JSON.stringify(tag_id))[0].tag_id;
+
+    await query('insert into chapter (uid,title,tag_id,context,chdate,isShare) values(?,?,?,?,?,?)',[uid,title,tag_id,context,chdate,isShare]);
+    
+    res.send({msg:'success'});
+});
+
+module.exports = router;
---
+++ project/server/api/personal.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var query = require('../db');
+const router = express.Router();
+// 个人信息页api
+
+router.post('/',async (req,res)=>{
+    // 请求需携带的参数 uid
+    const {uid} = req.body;
+    // const uid = 'k3i297def';
+    
+    var result = await query('select uname,headimg,signature from user where uid=?',[uid]);
+    result = JSON.parse(JSON.stringify(result))[0];
+
+    res.send(result);
+    // {"uname":"张三","headimg":"../images/zhangsan.jpg","signature":"这个人很懒，什么都没有写"}
+});
+
+module.exports = router;
