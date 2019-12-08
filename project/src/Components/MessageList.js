51b4e0fa228698389f4082ef73bd3bd069693ece
zhangwenkang6 <823809867@qq.com>
2019-12-08 16:58:15 +0800

    消息列表页

--- project/src/Components/MessageList.js
+++ project/src/Components/MessageList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Icon} from 'antd-mobile';
+import {Icon,NavBar} from 'antd-mobile';
 import '../css/messageList.css';
 import store from './UserId';
 
@@ -8,20 +8,20 @@ export default class MessageList extends Component {
         super(props)
         this.state={
             data:[
-                {img_path:'images/sort-test1.jpg',other:'有纪团队',con_id:'001'},           
-                {img_path:'images/sort-test2.jpg',other:'tangtang',con_id:'002'},
+                {img_path:'images/sort-test1.jpg',other:'有纪团队',con_id:'001',time:'12:21',last:'你好'},           
+                {img_path:'images/sort-test2.jpg',other:'tangtang',con_id:'002',time:'5:06',last:'hello world'},
             ],
         }
     }
 
     componentDidMount(){
-        const user =store.getState();
-        console.log(user);
-        if(user.login == 'success'){
-            console.log(1);
-        }else{
-            console.log(2);
-        }
+        // const user =store.getState();
+        // console.log(user);
+        // if(user.login == 'success'){
+        //     console.log(1);
+        // }else{
+        //     console.log(2);
+        // }
         // fetch('')
         // .then((res)=>res.json())
         // .then((res)=>{
@@ -33,17 +33,27 @@ export default class MessageList extends Component {
     render() {
         return (
             <div>
-                <div style={{width:'100%',backgroundColor:'#F8F8F8',height:'60px'}}>
-                    <Icon type="left" className='msg-left' onClick={() => window.location='/details'} 
+                {/* <div style={{width:'100%',backgroundColor:'#F8F8F8',height:'60px'}}>
+                    <Icon type="nordered-list" className='msg-left' onClick={() => window.location='/details'} 
                         style={{color:'#000',float: 'left',width: '10%',marginTop:'27px'}}/>
                     <p className='msg-title' style={{color:'#000',fontSize:'16px'}}>消息列表</p>
-                </div>
+                </div> */}
+                <NavBar
+                mode="light"
+                onLeftClick={() => window.location='/details'}
+                leftContent={[
+                    <img key='0' className='msg-title'></img>
+                ]}
+                style={{paddingTop:'10px',width:'100%',backgroundColor:'#FAA755',color:'#fff'}}
+                >消息列表</NavBar>
                 <div className='msg-list'>
                 {
                     this.state.data.map((tag,idx)=>
                     <li key={idx} className='msg-li' style={{width:'100%',height:'66px'}} onClick={() => window.location='/letter/'+tag.con_id}>
                         <div className="msg-img" style={{background:"url(" + require("../" +tag.img_path) + ")"}}></div>
+                        <p className='msg-time'>{tag.time}</p>
                         <p className='msg-text'>{tag.other}</p>
+                        <p className='msg-last'>{tag.last}</p>        
                     </li>)
                 }
                 </div>
