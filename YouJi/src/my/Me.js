837bc041b1c20518bc2fd8ee428a857c5ea01312
LiuXinmeng <1013430695@qq.com>
2020-04-23 09:41:09 +0800

    update

--- YouJi/src/my/Me.js
+++ YouJi/src/my/Me.js
@@ -11,6 +11,7 @@ export default class Me extends Component {
         super(props);
         this.state = {
           selectedTab: 'redTab',
+          data:{uname:'张三',uemail:'zhangsan@qq.com',userCounts:5,chapterCounts:5,signature:'我是张三',headimg:'images/timg.jpg',usex:'男'},
         };
       }
     // constructor(){
@@ -75,26 +76,7 @@ export default class Me extends Component {
     //         })
     //     })
     // }
-    // follow(){
-    //     let orange = document.getElementById('me-essay-orange')
-    //     orange.style.display='none';
-    //     let center = document.getElementById('find-center');
-    //     center.style.display='none';
-    //     let orange1 =document.getElementById('me-follow-orange');
-    //     orange1.style.display='block'
-    //     let center1 = document.getElementById('follow-center');
-    //     center1.style.display='block';
-    // }
-    // essay(){
-    //     let orange = document.getElementById('me-essay-orange')
-    //     orange.style.display='block';
-    //     let center = document.getElementById('find-center');
-    //     center.style.display='block';
-    //     let orange1 =document.getElementById('me-follow-orange');
-    //     orange1.style.display='none';
-    //     let center1 = document.getElementById('follow-center');
-    //     center1.style.display='none'
-    // }
+
     onChangeTab(tabName) {
         this.setState({
           selectedTab: tabName,
@@ -119,22 +101,22 @@ export default class Me extends Component {
                             }} size={30} name="bars"
                         />
                         </View>
-                        <Text style={styles.me_top_user}>张三</Text>
-                        <Text style={styles.me_top_email}>zhangsan@qq.com</Text>
+                        <Text style={styles.me_top_user}>{this.state.data.uname}</Text>
+                        <Text style={styles.me_top_email}>{this.state.data.uemail}</Text>
                 </View>
                 <View style={styles.me_title}>
                     <Image style={styles.me_head} source={require('../images/pic1.jpg')}/>
                     <View style={styles.me_num}>
                         <View style={styles.me_sex}>
                             {/* <Image></Image> */}
-                            <Text>女</Text>
+                            <Text>{this.state.data.usex}</Text>
                         </View>
                         <View style={styles.me_atten}>
-                            <Text>1</Text>
+                            <Text>{this.state.data.userCounts}</Text>
                             <Text>关注</Text>
                         </View>
                         <View style={styles.me_collect}>
-                            <Text>20</Text>
+                            <Text>{this.state.data.chapterCounts}</Text>
                             <Text>收藏</Text>
                         </View>
                     </View>
@@ -143,7 +125,7 @@ export default class Me extends Component {
                         <Button title="编辑资料" color="#faa755" onPress={()=>Actions.edit()}/>
                 </View>
                 <View style={styles.me_sign}>
-                    <Text>个性签名：这个人很懒，什么都没有写</Text>
+                    <Text>个性签名：{this.state.data.signature?this.state.data.signature:'这个人很懒，啥都没写'}</Text>
                 </View>
                 <View style={styles.me_nav}>
                     <TabBar style={styles.me_bar}
