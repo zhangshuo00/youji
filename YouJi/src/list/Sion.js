5ce474bbaf937f041cc87b4694871260d8b167fc
shaokuangkuang <314462224@qq.com>
2020-05-24 09:30:02 +0800

    muutetut rivit

--- YouJi/src/list/Sion.js
+++ YouJi/src/list/Sion.js
@@ -1,3 +1,146 @@
+// import React, { Component } from 'react'
+// import { Text, View,StyleSheet,Dimensions,Image, FlatList, TouchableOpacity,Alert, ScrollView,AsyncStorage} from 'react-native'
+// import { Actions } from 'react-native-router-flux';
+// import Icon from 'react-native-vector-icons/AntDesign';
+// const {width,scale} = Dimensions.get('window');
+// const s = width / 640;
+
+// export default class Sion extends Component {
+//     constructor(){
+//         super();
+//         this.state={
+//             datas:[],
+//             chid:'',
+//         }
+//     }
+//     //长按删除
+//     touchStart(e){
+//         let that=this;
+//             Alert.alert('提示',
+//                 '是否删除该分类，并删除该分类下所有笔记?', 
+//                 [
+//                 { text: '取消', onPress: () => console.log('cancel') },
+//                 { text: '确定', onPress: () => that.delTags(e)
+//                 },
+//                 ])
+//     }
+//     delTags=(e)=>{
+//         console.log(e)
+//         const post ={
+//             uid:k3mimknra,
+//             tags:e
+//         }
+//         console.log(post);
+//         fetch('/delTags',{
+//             method:'POST',
+//             // mode:'cors',
+//             headers: {'Content-Type': 'application/json'},
+//             body:JSON.stringify(post)
+//         })
+//         .then(res=>res.json())
+//         .then(data=>{
+//             console.log(data);
+//             window.location.reload(); //重新刷新该页面
+//             // this.setState({·
+//             //     datas:data
+//             // })
+//             // 根据返回的消息，渲染响应的页面
+//         })
+//     }
+
+//     async componentDidMount(){
+//         // AsyncStorage.setItem('chid',"20")
+//         const post ={
+//             uid:await AsyncStorage.getItem('uid').then(res=>res),
+//             tags:this.props.tag
+//             // tags:await AsyncStorage.getItem('tags').then(res=>res),
+//             // tags:"美食",
+//         }
+//         AsyncStorage.setItem('tags',this.props.tag)
+//         fetch('http://majia.hbsdduckhouse.club/Sion',{
+//             method:'POST',
+//             headers: {'Content-Type': 'application/json'},
+//             body:JSON.stringify(post)
+//         })
+//         .then(res=>res.json())
+//         .then(data=>{
+//             console.log(data,'返回的单页数据'),
+//             this.setState({
+//                 datas:data,
+//             })
+//         })  
+//     }
+
+
+    
+//     //跳转相信分类
+//     jumpToSionple =(item)=>{
+//         AsyncStorage.setItem('chid',""+item.chid+"")
+//         console.log('跳转到子页',item.chid)
+//         Actions.sionple()
+//      }
+    
+
+//     render() {
+//         return (
+//             <ScrollView>
+//                 <View style={{flexDirection:'row',backgroundColor:'rgb(250, 167, 85)',paddingTop:10,paddingBottom:10}}>
+//                     <TouchableOpacity style={styles.headIcon} onPress={()=>Actions.pop()}><Icon name='left' color={'white'} size={28}></Icon></TouchableOpacity>
+//                         <Text style={styles.headText}>{this.props.tag}</Text>
+//                     <TouchableOpacity style={styles.headIcon} onPress={()=>Actions.sionnew()}><Icon name='plus' color={'white'} size={28}></Icon></TouchableOpacity>
+//                 </View>
+//                 <View style={{
+//                     flexDirection: 'row',
+//                     alignItems: 'center'}}>
+//                         <FlatList
+//                         numColumns='2'
+//                         data={this.state.datas}
+//                         renderItem={({item})=>{
+//                             return(
+//                                 <TouchableOpacity 
+//                                     onLongPress={()=>this.touchStart(item)}
+//                                     onPress={()=>this.jumpToSionple(item)}
+//                                     style={{
+//                                         paddingTop:40*s,
+//                                         width:'50%',
+//                                         flexDirection:'column',
+//                                         alignItems:'center',
+//                                     }}>
+//                                 <View style={{flexDirection:'column',alignItems:'center',}}>
+//                                     <Image 
+//                                     source={{uri:'https://www.hbsdduckhouse.club/' + item.ch_headimg}}
+//                                     style={{height:120*s,width:150*s,borderRadius: 10*s}}/>
+//                                     <Text  style={{marginBottom:5*s}}>{item.title}</Text>
+//                                     <Text>{item.chdate}</Text>
+//                                 </View>
+//                                 </TouchableOpacity>
+//                             )
+//                             }}
+//                             />
+//                 </View>
+//             </ScrollView>
+//         )
+//     }
+// }
+
+
+// const styles = StyleSheet.create({
+//     headText:{
+//         marginRight:width*0.12,
+//         width:width*0.54,
+//         textAlign:'center',
+//         fontSize:22,
+//         color:'white'
+//     },
+//     headIcon:{
+//         marginLeft:width*0.02,
+//         width:width*0.2,
+//     },
+//     msgList:{
+//         width: width,
+//     },
+// })
+
 import React, { Component } from 'react'
 import { Text, View,StyleSheet,Dimensions,Image, FlatList, TouchableOpacity,Alert, ScrollView,AsyncStorage} from 'react-native'
 import { Actions } from 'react-native-router-flux';
--- YouJi/src/list/Sionnew.js
+++ YouJi/src/list/Sionnew.js
@@ -260,3 +260,267 @@ const styles =StyleSheet.create({
       transform: [{scale:0.95}],
   },
 })
+
+
+// import React, { Component } from 'react'
+// import { Text, View, TextInput,Dimensions,StyleSheet,TouchableOpacity,Image,AsyncStorage,FlatList,ToastAndroid} from 'react-native'
+// import ImagePicker from 'react-native-image-crop-picker'
+// import Icon from 'react-native-vector-icons/AntDesign';
+// import { Actions } from 'react-native-router-flux';
+// import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons'
+// import Icon4 from 'react-native-vector-icons/Entypo'
+// import Icon6 from 'react-native-vector-icons/Fontisto'
+
+// const {width,scale,height} = Dimensions.get('window');
+// const s = width / 640;
+// export default class Sionnew extends Component {
+//     constructor(){
+//         super();
+//         this.state = {
+//             imageUrl:require('../images/setBack.png'),
+//             uid:'',
+//             tags:'',
+//             title:'点击添加标题',
+//             context:'点击添加文本',
+//             checked: false,
+//             lists:[],
+//             uplists:[],
+//         }
+//     }
+
+
+// ///////////////////////////////////
+//     choosepics = ()=>{
+//         ImagePicker.openPicker({
+//             multiple: true,
+//             includeBase64:true
+//         }).then(images => {
+//             var lists = this.state.lists;//图片地址
+//             var uplists = this.state.uplists;//图片base64编码
+//             for(var i in images){
+//                 lists.push(images[i].path);
+//                 uplists.push( 'data:image/jpeg;base64,' + images[i].data);
+//             }
+//             this.setState({
+//                 lists:lists,
+//                 uplists:uplists
+//             })
+//             console.log(this.state.lists)
+//         });
+//         ImagePicker.clean().then(() => { 
+//             console.log('removed all tmp images from tmp directory');
+//         }).catch(e => { 
+//             console.log(e)
+//         });
+//     }
+//     takepic = ()=>{
+//         ImagePicker.openCamera({
+//             width:300,
+//             height:400,
+//             cropping:true,
+//             includeBase64:true
+//         }).then(image=>{
+//             var lists = this.state.lists;
+//             var uplists = this.state.uplists;
+//             lists.push(image.path);
+//             uplists.push(image.data);
+//             this.setState({
+//                 lists:lists,
+//                 uplists:'data:image/jpeg;base64,' + images[i].data
+//             })
+//         });
+//         ImagePicker.clean().then(() => { 
+//             console.log('removed all tmp images from tmp directory');
+//         }).catch(e => { 
+//             console.log(e)
+//         });
+//     }
+//     rechoosepics = ()=>{
+//         this.setState({
+//             lists:[],
+//             uplists:[]
+//         })
+//     }
+
+
+
+//       addSion=async()=>{
+//         var imgurl = this.state.lists;
+//         const post ={
+//             uid:await AsyncStorage.getItem('uid').then(res=>res),
+//             tags:await AsyncStorage.getItem('tags').then(res=>res),
+//             title:this.state.title,
+//             context:this.state.context,
+//             imgData:this.state.uplists,
+//             isShare: this.state.checked ? 1 : 0
+//           }
+//           console.log(post,'posi');
+//         fetch('http://majia.hbsdduckhouse.club/addSionple',{
+//             method:'POST',
+//             // mode:'cors',
+//             headers: {'Content-Type': 'application/json'},
+//             body:JSON.stringify(post)
+//         })
+//         .then(res=>res.json())
+//         .then(data=>{
+//           if(data.msg==='success' ){
+//             alert('保存成功','', [
+//               { text: '确定', onPress: () => console.log('cancel') },
+//               ])
+//           }
+//             })
+//       }
+
+
+
+//     render() {
+//         return (
+//             <View>
+//                 <View style={{flexDirection:'row',backgroundColor:'rgb(250, 167, 85)',paddingTop:10,paddingBottom:10}}>
+//                     <TouchableOpacity style={styles.headIcon} onPress={()=>Actions.pop()}><Icon name='left' color={'white'} size={28}></Icon></TouchableOpacity>
+//                     <Text style={styles.headText}>新建笔记</Text>
+//                     <TouchableOpacity style={styles.headIcon} onPress={()=>{this.addSion()}}>
+//                         <Text style={{color:'white',fontSize:18}}>保存</Text>
+//                     </TouchableOpacity>
+//                 </View>
+//                 <View style={{ justifyContent:'center', marginTop:30*s }}>
+//                     <TextInput 
+//                         placeholder='点击添加标题'
+//                         textAlignVertical="top"
+//                         multiline={true}
+//                         onChangeText={(text)=>{
+//                           this.setState({
+//                             title:text
+//                           })
+//                         }}
+//                         style={{ fontSize:30*s,  borderStyle: "solid",width:"100%",height: 80*s,padding: 0,marginBottom:20,paddingLeft: 10,backgroundColor:'#fff'}}/>
+//                     <TextInput 
+//                         placeholder='点击添加内容'
+//                         textAlignVertical="top"
+//                         multiline={true}
+//                         onChangeText={(text)=>{
+//                             this.setState({
+//                               context:text
+//                             })
+//                           }}
+//                         style={{fontSize:30*s,borderStyle: "solid",width:"100%",height: 300*s,paddingLeft: 10,backgroundColor:'#fff'}}/>
+//                 </View>
+//                 <View style={styles.picchoose}>
+//                                 <Text style={styles.pictext}>添加图片</Text>
+//                                 {/* //三个图标按钮 */}
+//                                 <TouchableOpacity onPress={this.choosepics}>
+//                                     <Icon2 size={45*s} style={styles.iconpic} name='image-plus'/>
+//                                 </TouchableOpacity>
+//                                 <Text style={{width:0.01*width,}}></Text>
+//                                 <TouchableOpacity onPress={this.takepic}>
+//                                     <Icon4 size={40*s} style={styles.iconpic} name='camera'/>
+//                                 </TouchableOpacity>
+//                                 <Text style={{width:0.03*width,}}></Text>
+//                                 <TouchableOpacity onPress={this.rechoosepics}>
+//                                     <Icon6 size={35*s} style={styles.doiconpic} name='redo'/>
+//                                 </TouchableOpacity>
+//                             </View>
+//                             <FlatList 
+//                                 style={styles.picbox}
+//                                 data={this.state.lists}
+//                                 numColumns={3}
+//                                 ListFooterComponent={
+//                                     <View style={{
+//                                         height:0.03*width
+//                                     }}>
+//                                     </View>
+//                                 }
+//                                 renderItem={({item})=>(
+//                                     <Image
+//                                         style={styles.pics}
+//                                         resizeMode="cover"
+//                                         source={{uri:`${item}`}}
+//                                     />
+//                                 )}
+//                                 />  
+//             </View>
+//         )
+//     }
+// }
+// const styles =StyleSheet.create({
+//     TextStyle:{
+//         marginTop:20*s,
+//         marginRight:30*s,
+//         fontSize:30*s
+//     },
+//     ButtonStyle:{
+//         width:300*s,
+//         height:50*s,
+//         paddingTop:8*s,
+//         backgroundColor:"orange",
+//         color:'#fff',
+//         marginTop:50*s,
+//         marginLeft:"14%",
+//         borderRadius:20*s
+//     },
+//     TextStyle:{
+//       marginTop:20*s,
+//       marginRight:30*s,
+//       fontSize:30*s
+//     },
+//     headText:{
+//         marginRight:width*0.12,
+//         width:width*0.54,
+//         textAlign:'center',
+//         fontSize:22,
+//         color:'white'
+//     },
+//     headIcon:{
+//         marginLeft:width*0.02,
+//         width:width*0.2,
+//     },
+//     msgList:{
+//         width: width,
+//     },
+
+
+//     picchoose:{
+//       height:0.05*height,
+//       paddingTop:0.01*height,
+//       paddingBottom:0.01*height,
+//       borderRadius:10,
+//       flexDirection:'row',
+//   },
+//   pictext:{
+//       width:0.3*width,
+//       height:0.04*height,
+//       color:'#555',
+//       fontSize:25*s,  
+//       textAlign:'center' ,
+//       textAlignVertical:'center', 
+//   },
+//   doiconpic:{
+//       width:0.1*width,
+//       height:0.04*height,
+//       textAlignVertical:'center',
+//       textAlign:'center',
+//       color:'#555'
+//   },
+//   iconpic:{
+//       width:0.15*width,
+//       textAlign:'center',
+//       height:0.04*height,
+//       textAlignVertical:'center',
+//       color:'#888'
+//   },
+//   picbox:{
+//       height:0.6*height,
+//       backgroundColor:'#fff',
+//       padding:0.01*width,
+//       width:1*width,
+//       transform: [{scale:0.98}],
+//       marginRight:'auto',
+//       marginLeft:'auto',
+//   },
+//   pics:{
+//       width:0.275*width,
+//       height:0.18*height,
+//       margin:0.007*width,
+//       transform: [{scale:0.95}],
+//   },
+// })
