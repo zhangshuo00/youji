6586cdc592d093aa8907cde50a52068537b2fd5a
shaokuangkuang <314462224@qq.com>
2020-05-25 08:06:45 +0800

    add 十一周目标

--- YouJi/index.js
+++ YouJi/index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import Sionnew from './src/list/Sionnew';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
